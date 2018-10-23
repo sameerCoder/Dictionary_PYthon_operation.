99272dfb32ece3b51c0413beac0bc577d6ae4f47
sameerCoder <39183783+sameerCoder@users.noreply.github.com>
2018-10-23 17:22:42 +0530

    Add files via upload

---
+++ Dictionary_basic_function..py
@@ -0,0 +1,12 @@
+#This Program code basic function of dictionary taking 0(1) as key value pair is there.
+
+dict={'joe':10,"don":20,"son":30,"car":40}
+
+print("Before updating key value :",dict['joe'])
+
+dict['joe']=15
+print("After updating key value :",dict['joe'])
+
+print("All keys of dictionary :",dict.keys())
+
+print("All the items of dictionary (key as well as value :)", dict.items())
